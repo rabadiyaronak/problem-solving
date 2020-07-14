0dfb66bf1b619c9630be7f7501fbdda620967b01
rabadiyaronak <rabadiyaronak@gmail.com>
2020-07-14 13:48:44 +0530

    add power of two

---
+++ src/main/java/leetcode/PowerOfTwo.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean isPowerOfTwo(int n) {
+
+		if (n == 0) {
+			return false;
+		}
+
+		while ((n & 1) != 1) {
+			n = n >> 1;
+		}
+
+		return n == 1;
+	}
+}
