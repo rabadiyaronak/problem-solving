85eef23b08a9917de83fc7627cf7ac5792bb40db
rabadiyaronak <rabadiyaronak@gmail.com>
2020-07-15 15:19:02 +0530

    add power of three

---
+++ src/main/java/leetcode/PowerOfThree.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class PowerOfThree {
+	public static void main(String[] args) {
+		boolean result;
+		result = new PowerOfThree().isPowerOfThree(1);
+		System.out.println(result);
+		result = new PowerOfThree().isPowerOfThree(3);
+		System.out.println(result);
+		result = new PowerOfThree().isPowerOfThree(9);
+		System.out.println(result);
+		result = new PowerOfThree().isPowerOfThree(27);
+		System.out.println(result);
+		result = new PowerOfThree().isPowerOfThree(51);
+		System.out.println(result);
+	}
+
+	public boolean isPowerOfThree(int n) {
+		if ((n & 1) == 1) {
+			double pow = 1;
+			while (pow < n) {
+				pow = pow * 3;
+			}
+			return pow == n;
+		}
+		return false;
+
+	}
+}
